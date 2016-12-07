c32baeeb303161b993a9bdd8334ff665e6731796
Kenneth Louis <lowkam4@gmail.com>
2016-12-07 14:37:45 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
       $("#like").html("You like this project!");
+         $("#likeIII").html("You like this project!");
+         $("#likeII").html("You like this project!");
+         $("#likeIV").html("You like this project!");
     });
     
     
